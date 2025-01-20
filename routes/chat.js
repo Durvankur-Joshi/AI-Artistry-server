fcd8531960e2648828a47547d03e4914949a8777
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-01-20 13:14:33 +0530

    --

--- routes/chat.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { chatbotResponse } from "../controllers/chatController.js";
-
-const router = express.Router();
-
-router.post("/chat", chatbotResponse);
-
-export default router;
